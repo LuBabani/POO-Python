c88d7d2a7acd6623f8ec6efbb67c69060f57867c
Lu Babani <72316961+LuBabani@users.noreply.github.com>
2023-01-09 14:38:11 -0300

    Update automoveis.py

--- automoveis.py
+++ automoveis.py
@@ -1,4 +1,6 @@
 class Automoveis:
+    global ACELERACAO
+    ACELERACAO = 10
 
     #Metodo construtor
     def __init__(self, cores, fabricante, data, max, mod, cambio=False ):
@@ -15,10 +17,22 @@ class Automoveis:
     #metodos
     def ligar(self):
         if self.ligado and self.velo == 0:
-              self.ligado = False #desligar o carro
+            self.ligado = False #desligar o carro
+            return True
+        if not self.ligado:
+            self.ligado = True
+            return False
+        input('Pressione qualquer tecla para continuar')
 
-    def acelerar():
-    pass
+    def acelerar(self):
+        if (self.veloMax) >= (self.velo + ACELERACAO):
+            self.velo = self.velo + ACELERACAO
+        else:
+            self.velo = self.veloMax
 
-    def frear():
-    pass
+
+    def frear(self):
+        if (self.velo-ACELERACAO) >=0:
+            self.velo -= ACELERACAO
+        else:
+            self.velo = 0    
