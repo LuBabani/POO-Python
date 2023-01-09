d8a7ff47a9601a5fa2da91787e3f211803d7273e
Lu Babani <72316961+LuBabani@users.noreply.github.com>
2023-01-09 15:41:51 -0300

    add metodo mostrarMarcha()
    Verificando qual marcha o carro está.

--- automoveis.py
+++ automoveis.py
@@ -24,15 +24,30 @@ class Automoveis:
             return False
         input('Pressione qualquer tecla para continuar')
 
+    def mostrarMarcha(self):
+        if self.velo <=40:
+            return "1a"
+        elif self.velo <=80:
+            return "2a"
+        elif self.velo <=120:
+            return "3a"
+        else:
+            return "4a"
+
     def acelerar(self):
         if (self.veloMax) >= (self.velo + ACELERACAO):
             self.velo = self.velo + ACELERACAO
+
         else:
             self.velo = self.veloMax
+
+        if self.cambioAuto == True:
+            return Automoveis.mostrarMarcha(self)
 
 
     def frear(self):
         if (self.velo-ACELERACAO) >=0:
             self.velo -= ACELERACAO
         else:
-            self.velo = 0    
+            self.velo = 0 
+
